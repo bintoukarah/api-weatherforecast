c9def2f71cd45e23abff4e6e2efbad11ab058a28
bkaramo <bintoukaramoko3@gmail.com>
2022-05-13 15:55:18 +0200

    added function display week

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
     let updateButton = document.getElementById("submit");
+
     updateButton.addEventListener("click", (event) => { //au click 
         event.preventDefault();
-        let nomVille = document.getElementById("city").value; //recuper nom de ville
+
+        let nameOfCity = document.getElementById("city").value; //recuper nom de ville
         const API_KEY = "45cb0e6d117446dabe3fb0c56ffa888a" 
-        //rechercher 
-        let URL = `https://api.opencagedata.com/geocode/v1/json?q=${nomVille}&key=${API_KEY}&language=fr&pretty=1`
+        
+        // requete à l'API OCD
+        let URL = `https://api.opencagedata.com/geocode/v1/json?q=${nameOfCity}&key=${API_KEY}&language=fr&pretty=1`
         fetch(URL)
             .then(response => {
                 if (response.status == 200) { // on vérifier que l'appel à l'API a fonctionné
@@ -14,25 +17,14 @@ document.addEventListener("DOMContentLoaded", function () {
                 else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
             })
             .then(data => {
-                const myDate = new Date();
-                const curr_day = myDate.getDay(); //extraire l'index de ma journee}
-                let d_names = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday", "Monday", "Tuesday", "Wednesday",
-                    "Thursday", "Friday", "Saturday"];
-                let theDay = d_names[curr_day];//associer l'index au nom de ma journee
-                // afficher une journée
-                // document.querySelector('#printDay').innerHTML = `${theDay}`  
-                // afficher  5jours de la semaine a partir d'aujourd'hui:
-                for(let i = 0; i < 5 ; i++){
-                    theDay = d_names[curr_day + i];
-                     console.log(theDay);
-                     let element = document.createElement("h3");
-                     let newDiv = document.getElementById("printWeek");
-                     newDiv.appendChild(element).innerHTML = `${theDay}`;
-                 }
+
                 let donGeo = data.results[0].geometry;// extraire lattitude et longitude de ma ville
-                console.log(donGeo);
+                // console.log(donGeo);
                 let lat = donGeo.lat;//latitude
                 let lon = donGeo.lng;//longitude
+
+
+                // requete à l'API OWM pour recup la meteo a partir des coords de la ville
                 const APIKEY = "27e489831f7a566877884878940a4f9d";
                 let URL2 = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${APIKEY}`//utiliser les donnees geo or avoir ma meteo
                 fetch(URL2)
@@ -43,25 +35,52 @@ document.addEventListener("DOMContentLoaded", function () {
                         else console.log(`Erreur lorsqu'on a tenté de récupérer les data`);
                     })
                     .then(data => {
+                        // let meteo = data.current.weather[0].id; // recuperer la meteo du jour 
+
+                        // getDay()
+                        const myDate = new Date();
+                        const currentDay = myDate.getDay(); //extraire l'index de ma journee}
+                        let myWeek = ["Sunday", "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday",
+                                    "Sunday", "Monday", "Tuesday", "Wednesday",
+                            "Thursday", "Friday", "Saturday"];
+                        let theDay = myWeek[currentDay];//associer l'index au nom de ma journee
                         
-                        let meteo = data.current.weather[0].id; // recuperer la meteo de mon l'APi
-                        console.log(meteo);
-                        //afficher l'icone  de la meteo relatif a l'id 
-                        if (meteo == 800) {
-                            document.querySelector('#printIcon').src = "assets/weathers/sun.svg"; 
-                        } else if (meteo >= 600 && meteo <= 622) {
-                            document.querySelector('#printIcon').src = `assets/weathers/snow.svg`;
-                        } else if (meteo > 800) {
-                            document.querySelector('#printIcon').src = `assets/weathers/clouds.svg`;
-                        } else if (meteo >= 200 && meteo <= 531) {
-                            document.querySelector('#printIcon').src = `assets/weathers/rain.svg`;
-                        } else if (meteo >= 701 && meteo <= 781) {
-                            document.querySelector('#printIcon').src = `assets/weathers/cloudy.svg`;
+                        // afficher  5jours de la semaine a partir d'aujourd'hui:
+                        for(let i = 0; i < 5 ; i++){
+                            let meteo = data.daily[i].weather[0].id // meteo du jour 0
+                            theDay = myWeek[currentDay + i];
+                            let element = document.createElement("h3");
+                            let img = document.createElement("img");
+
+                            let newDiv = document.getElementById("printWeek");
+                            newDiv.appendChild(element).innerHTML = `${theDay}`;
+                            
+
+                            //afficher l'icone  de la meteo relatif a l'id
+                            
+                            if (meteo == 800) {
+                                newDiv.appendChild(img).src = "assets/weathers/sun.svg";
+                                console.log(meteo);
+                            } else if (meteo >= 600 && meteo <= 622) {
+                                newDiv.appendChild(img).src = `assets/weathers/snow.svg`;
+                                console.log(meteo)
+                            } else if (meteo > 800) {
+                                newDiv.appendChild(img).src = `assets/weathers/clouds.svg`;
+                                console.log(meteo);
+                            } else if (meteo >= 200 && meteo <= 531) {
+                                newDiv.appendChild(img).src = `assets/weathers/rain.svg`;
+                                console.log(meteo);
+                            } else if (meteo >= 701 && meteo <= 781) {
+                                newDiv.appendChild(img).src = `assets/weathers/cloudy.svg`;
+                                console.log(meteo);
+                            }
                         }
+                        
                     })
+             
                     .catch(err => console.log(err))
+              
             })
             .catch(err => console.log(err))
     });
-
-});
+})
